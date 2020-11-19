006c50ef65d396627e4dde1652ccd269ebb83197
Michal Sustr <michal.sustr@aic.fel.cvut.cz>
2020-11-19 18:23:35 +0100

    Implement &operator[]

--- open_spiel/algorithms/infostate_cfr.h
+++ open_spiel/algorithms/infostate_cfr.h
@@ -71,6 +71,12 @@ class DecisionVector<CFRInfoStateValues> {
     SPIEL_DCHECK_LT(decision_id.id(), vec_.size());
     return vec_[decision_id.id()];
   }
+  const CFRInfoStateValues& operator[](const DecisionId& decision_id) const {
+    SPIEL_DCHECK_TRUE(decision_id.BelongsToTree(tree_));
+    SPIEL_DCHECK_LE(0, decision_id.id());
+    SPIEL_DCHECK_LT(decision_id.id(), vec_.size());
+    return vec_[decision_id.id()];
+  }
   auto begin() { return vec_.begin(); }
   auto end() { return vec_.end(); }
 };
