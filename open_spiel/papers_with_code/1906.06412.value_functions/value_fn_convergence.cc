803e21125454207e1ea558869fe6e7b31d2a3cc9
Michal Sustr <michal.sustr@aic.fel.cvut.cz>
2020-12-11 15:22:17 +0100

    Add value fn convergence for CFR.

--- open_spiel/papers_with_code/1906.06412.value_functions/value_fn_convergence.cc
+++ open_spiel/papers_with_code/1906.06412.value_functions/value_fn_convergence.cc
@@ -19,8 +19,9 @@
 #include "open_spiel/abseil-cpp/absl/flags/usage.h"
 #include "open_spiel/abseil-cpp/absl/flags/parse.h"
 
-ABSL_FLAG(std::string, game_name, "matrix_biased_mp", "Game to run.");
-ABSL_FLAG(int, depth, 1, "Max depth of the trunk.");
+ABSL_FLAG(std::string, game_name, "kuhn_poker", "Game to run.");
+ABSL_FLAG(int, depth, 3, "Max depth of the trunk.");
+ABSL_FLAG(std::string, evaluator, "oracle", "Which evaluator to use");
 
 #include "open_spiel/algorithms/ortools/dl_oracle_evaluator.h"
 #include "open_spiel/games/goofspiel.h"
@@ -56,7 +57,41 @@ void RunTrunkIterationsWithValueOracle(std::string game_name, int depth) {
   int num_iters = 10;
 
   std::cout << "iters,cur_expl,avg_expl" << std::endl;
-  for (int i = 0; i < 10000; ++i) {
+  for (int i = 0; i < 1000; ++i) {
+    double cur_expl = TrunkExploitability(&whole_game, *current_policy);
+    double avg_expl = TrunkExploitability(&whole_game, *average_policy);
+    std::cout << i * num_iters << ","
+              << cur_expl << ","
+              << avg_expl << std::endl;
+    dl_solver.RunSimultaneousIterations(num_iters);
+  }
+}
+
+void RunTrunkIterationsWithCfrEvaluator(std::string game_name, int depth) {
+  std::shared_ptr<const Game> game = LoadGame(game_name);
+
+  std::shared_ptr<const dlcfr::LeafEvaluator> terminal_evaluator =
+      dlcfr::MakeTerminalEvaluator();
+  std::shared_ptr<Observer> public_observer =
+      game->MakeObserver(kPublicStateObsType, {});
+  std::shared_ptr<Observer> infostate_observer =
+      game->MakeObserver(kInfoStateObsType, {});
+
+  auto leaf_evaluator = std::make_shared<dlcfr::CFREvaluator>(
+      game, 100, nullptr, terminal_evaluator,
+      public_observer, infostate_observer);
+  leaf_evaluator->num_cfr_iterations = 100;
+
+  dlcfr::DepthLimitedCFR dl_solver(
+      game, depth, leaf_evaluator, terminal_evaluator);
+
+  SequenceFormLpSpecification whole_game(*game);
+  auto current_policy = dl_solver.CurrentPolicy();
+  auto average_policy = dl_solver.AveragePolicy();
+  int num_iters = 10;
+
+  std::cout << "iters,cur_expl,avg_expl" << std::endl;
+  for (int i = 0; i < 1000; ++i) {
     double cur_expl = TrunkExploitability(&whole_game, *current_policy);
     double avg_expl = TrunkExploitability(&whole_game, *average_policy);
     std::cout << i * num_iters << ","
@@ -74,10 +109,18 @@ void RunTrunkIterationsWithValueOracle(std::string game_name, int depth) {
 int main(int argc, char** argv) {
   absl::SetProgramUsageMessage(
       "Experiment runner for the convergence of trunk strategies using "
-      "the counterfactually optimal value function. All games should have "
-      "exploitability approaching zero.");
+      "the counterfactually (optimal) value functions. "
+      "All games should have exploitability approaching zero.");
   absl::ParseCommandLine(argc, argv);
 
-  open_spiel::algorithms::ortools::RunTrunkIterationsWithValueOracle(
-      absl::GetFlag(FLAGS_game_name), absl::GetFlag(FLAGS_depth));
+  std::string evaluator = absl::GetFlag(FLAGS_evaluator);
+  if (evaluator == "oracle") {
+    open_spiel::algorithms::ortools::RunTrunkIterationsWithValueOracle(
+        absl::GetFlag(FLAGS_game_name), absl::GetFlag(FLAGS_depth));
+  } else if (evaluator == "cfr") {
+    open_spiel::algorithms::ortools::RunTrunkIterationsWithCfrEvaluator(
+        absl::GetFlag(FLAGS_game_name), absl::GetFlag(FLAGS_depth));
+  } else {
+    open_spiel::SpielFatalError("Exhausted pattern match: no such evaluator");
+  }
 }
