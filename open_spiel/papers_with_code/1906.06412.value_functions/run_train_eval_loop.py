404e1819675706d9f0b77f3abfb0697dbfe6cf30
Michal Sustr <michal.sustr@aic.fel.cvut.cz>
2021-01-26 17:33:39 +0100

    Update experiment runner.

--- open_spiel/papers_with_code/1906.06412.value_functions/run_train_eval_loop.py
+++ open_spiel/papers_with_code/1906.06412.value_functions/run_train_eval_loop.py
@@ -1,33 +1,30 @@
-import sweep
+import sweep as sweep
 
 # home = "/storage/praha1/home/sustrmic/"
-# backend = "meta"
+# backend = "meta",
 # backend_params = [
 #                    "-l", "select=1:ncpus=2:mem=4gb",
 #                    "-l", "walltime=08:00:00",
 #                    "-v", f"LD_LIBRARY_PATH="
 #                          f"{home}/open_spiel/open_spiel/libtorch/libtorch/lib/:"
 #                          f"{home}/open_spiel/open_spiel/ortools/lib/",
-#                  ]
+#                  ],
 
 home = "/home/sustr"
 backend = "parallel"
 backend_params = []
-experiment_name = "randomization_and_bandit"
+experiment_name = "eval_particles"
+
 
 def param_fn(param, context):
   if param == "game_name":
     return [
       "kuhn_poker",
-      # "leduc_poker",
-      # "goofspiel(players=2,num_cards=3,imp_info=True,
-      # points_order=descending)",
-      # "goofspiel(players=2,num_cards=4,imp_info=True,
-      # points_order=descending)",
+      "leduc_poker",
+      "goofspiel(players=2,num_cards=3,imp_info=True,points_order=descending)",
+      "goofspiel(players=2,num_cards=4,imp_info=True,points_order=descending)",
       "goofspiel(players=2,num_cards=5,imp_info=True,points_order=descending)",
     ]
-  elif param == "use_bandits_for_cfr":
-    return ["RegretMatchingPlus", "RMPlusWithEps"]
   elif param == "depth":
     if context["game_name"] == "kuhn_poker":
       return [3, 4]
@@ -38,34 +35,28 @@ def param_fn(param, context):
     elif "num_cards=4" in context["game_name"]:
       return [1, 2]
     elif "num_cards=5" in context["game_name"]:
-      return [2]
+      return [1, 2]
   elif param == "data_generation":
     return ["dl_cfr", "random"]
   elif param == "prob_pure_strat":
-    if context["data_generation"] == "dl_cfr":
-      return [0]
-    else:
-      return [0, 0.1, 0.2]
-
+    return [0.1]
 
 
 sweep.run_sweep(backend,
                 backend_params,
-                binary_path=f"{home}/experiments/train_eval_loop",
+                binary_path=f"{home}/experiments/train_eval_loop_particles",
                 base_output_dir=f"{home}/experiments/{experiment_name}",
                 base_params=dict(
                     cfr_oracle_iterations=100,
                     num_loops=300,
+                    use_bandits_for_cfr="RegretMatchingPlus",
                     trunk_eval_iterations="1,2,5,10,20,50,100",
                     train_batches=64,
                     num_trunks=400,
                     batch_size=-1,
-                    num_layers=5,
-                    num_width=6,
                 ),
                 comb_params=[
                   "game_name",
-                  "use_bandits_for_cfr",
                   "depth",
                   "data_generation",
                   "prob_pure_strat"
